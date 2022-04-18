07eddf17f0f107fbf04c8505ca9b1234d6b3d7fe
Professor Rodrigo Bossini <professorbossini@gmail.com>
2022-04-18 20:32:26 -0300

    Classe Calculadora faz soma, subtracao, multiplicacao e divisao. Classe de teste chama o somar

---
+++ Calculadora.java
@@ -0,0 +1,25 @@
+public class Calculadora{
+    //tipo de retorno
+    //nome
+    //lista de parâmetros
+    //corpo
+    //essa é a definição do método somar
+    //aqui eu disse que ele existe
+    public int somar(int a, int b){
+        return a + b;
+    }
+
+    public int subtrair(int a, int b){
+        return a - b;    
+    }
+
+    public int multiplicar(int a, int b){
+        return a * b;
+    }
+
+    public int dividir (int a, int b){
+        //divisão inteira
+        return a / b;
+    }
+
+}
---
+++ TesteCalculadora.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        int opcao;
+        String menu = "1-Soma\n2-Subtracao\n3-Multiplicacao\n4-Divisao\n";
+        opcao = Integer.parseInt(JOptionPane.showInputDialog(menu));
+        int operando1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro operando"));
+        int operando2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo operando"));
+        int resultado = 0;
+        //fall-through: lógica em queda
+        switch(opcao){
+            case 1:
+                //isso é uma chamada ao método somar
+                //aqui estou colocando ele em execução
+                resultado = calc.somar(operando1, operando2);
+                break;
+            
+
+        }
+        JOptionPane.showMessageDialog(null, "O resultado é: " + resultado);
+    }
+}
